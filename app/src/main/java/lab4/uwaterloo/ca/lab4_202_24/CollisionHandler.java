86cd7276dde7d5eb29d4208e49dd91a1d189cd81
PatzHum <patrick.hum@hotmail.com>
2017-03-15 18:40:21 -0400

    stuff

---
+++ app/src/main/java/lab4/uwaterloo/ca/lab4_202_24/CollisionHandler.java
@@ -0,0 +1,14 @@
+package lab4.uwaterloo.ca.lab4_202_24;
+
+/**
+ * Created by patri on 2017-03-15.
+ */
+
+public class CollisionHandler {
+    public static void ShiftBlocks(GestureCallback.Direction direction, GameBlock[] gameBlocks){
+
+    }
+
+
+
+}

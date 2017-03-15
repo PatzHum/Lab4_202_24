56cecf5ec7a2f00af25f20b02e8e038d8bd73138
PatzHum <patrick.hum@hotmail.com>
2017-03-15 19:08:33 -0400

    komit

--- app/src/main/java/lab4/uwaterloo/ca/lab4_202_24/CollisionHandler.java
+++ app/src/main/java/lab4/uwaterloo/ca/lab4_202_24/CollisionHandler.java
@@ -1,14 +1,30 @@
 package lab4.uwaterloo.ca.lab4_202_24;
 
+import android.util.Pair;
+
+import java.util.LinkedList;
+import java.util.Vector;
+
 /**
  * Created by patri on 2017-03-15.
  */
 
 public class CollisionHandler {
-    public static void ShiftBlocks(GestureCallback.Direction direction, GameBlock[] gameBlocks){
+    public static void ShiftBlocks(GestureCallback.Direction direction, LinkedList<GameBlock> gameBlocks){
+        Vector<Vector<Pair<Integer,Integer>>> grid = new Vector<>();
+        //val, gameblockindex
+        for (int i = 0; i < 4; ++i){
+            Vector<Pair<Integer, Integer>> line = new Vector<>();
+            for (int j = 0; j < 4; ++j){
+                line.add(new Pair<Integer, Integer>(0,0));
+            }
+            grid.add(line);
+        }
 
+        int gameBlockIndex = 0;
+        for (GameBlock gameBlock : gameBlocks){
+            grid.get(gameBlock.by).get(gameBlock.bx) = new Pair<>(gameBlock.)
+            gameBlockIndex++;
+        }
     }
-
-
-
 }
